9934c0627fa4a05f0262f86e386314ab246c1d06
Roshan kk <roshanochu09@gmail.com>
2025-02-14 18:31:25 +0530

    provider setup finished

--- lib/provider/category_provider.dart
+++ lib/provider/category_provider.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:money_tracker/db/category/category_db.dart';
 import 'package:money_tracker/models/category/category_model.dart';
@@ -7,13 +6,10 @@ import 'package:money_tracker/models/category/category_model.dart';
 class CategoryProvider extends ChangeNotifier {
   List<CategoryModel> incomeCategoryList = [];
   List<CategoryModel> expenseCategoryList = [];
-
   Future<void> refreshCategory() async {
     final hello = await CategoryDB.instance.categoryRefreshUI();
-
     incomeCategoryList.clear();
     expenseCategoryList.clear();
-
     for (int i = 0; i < hello.length; i++) {
       if (hello[i].type == CategoryType.income) {
         incomeCategoryList.add(hello[i]);
@@ -21,8 +17,12 @@ class CategoryProvider extends ChangeNotifier {
         expenseCategoryList.add(hello[i]);
       }
     }
-
     notifyListeners();
+  }
+
+  Future<void> insertCategoryProvider(CategoryModel categoryModelValue) async {
+    await CategoryDB.instance.insertCategory(categoryModelValue);
+    await refreshCategory();
   }
 
   Future<void> deleteCategoryProvider(String categoryID) async {
